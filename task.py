f18dea03d94c68f7d025380cb36b68eedb59359a
Uzoamaka Mbakogu <127051767+Amarky-C@users.noreply.github.com>
2023-04-04 21:38:43 +0100

    Update task.py

--- task.py
+++ task.py
@@ -16,12 +16,20 @@ print(bool(a!=b))  #returns true as a is not equal to b
 #Question 2 write a python function that takes 10list and 
 #returns true if they have at least one common member 
 
-Colors = ['blue', 'red', 'white', 'champagne', 'orange']
-Fruits = ['cherry', 'tangerine','orange', 'apple', 'banana']
-Phone = ['apple', 'infinix','orange','samsung', 'tecno']
-Drinks = ['vodka', 'orange','champagne', 'whiskey', 'liquor']
+Colors = ['blue', 'hisense', 'white', 'champagne', 'orange']
+Fruits = ['cherry', 'tangerine','orange', 'apple', 'hisense']
+Phone = ['hisense', 'infinix','orange','samsung', 'tecno']
+Drinks = ['vodka', 'orange','champagne', 'hisense', 'liquor']
+Laptop = ['samsung', 'apple','hisense', 'orange', 'hp']
+Makeup = ['mascara','orange','eyeliner', 'hisense','blush']
+Coffee = ['black', 'nescafe','orange', 'tetley','hisense']
+Software = ['python', 'orange', 'html', 'hisense', 'javascript']
+Tablet = ['hisense', 'android', 'xiaomi', 'orange', 'infinix']
+Conditioner = ['thermocool', 'orange', 'scanfrost', 'hisense']
+               
 
-common_elements = set(Colors).intersection(Fruits,Phone,Drinks)
+
+common_elements = set(Colors).intersection(Fruits,Phone,Drinks,Laptop,Makeup,Coffee)
 
 def cohort_four():
     if common_elements:
