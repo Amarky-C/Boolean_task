59535d7f5eccbc948a3b5d513b4eb9ee1df04f68
Uzoamaka Mbakogu <127051767+Amarky-C@users.noreply.github.com>
2023-04-07 20:53:37 +0100

    Update task1.py

--- task1.py
+++ task1.py
@@ -7,3 +7,6 @@ print(bool(a==b)) #returns false as a is not equal to b
 print(bool(a>b))  #returns false as a is not greater than b
 print(bool(a<b))  #returns true as a is greater than b
 print(bool(a!=b))  #returns true as a is not equal to b
+
+
+
